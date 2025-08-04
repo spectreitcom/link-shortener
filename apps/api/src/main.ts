b3e4a2f0586cfb61ea8cd6bc1927b5542d7f33c7
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2025-08-04 13:10:22 +0200

    init

---
+++ apps/api/src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+  await app.listen(process.env.PORT ?? 3003);
+}
+
+bootstrap()
+  .then(() => console.log('API is running!'))
+  .catch((e) => console.error(e));
